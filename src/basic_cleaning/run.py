29606751957e7a4d9200a7dea97642e0b9ce92ea
Pablo Rosales <parosales@gmail.com>
2024-02-20 12:38:45 -0600

    delimiting the longitude and latitude valid values in the basic_cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -42,6 +42,10 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     logger.info("Save artifact into W&B")
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
     artifact = wandb.Artifact(
         args.output_artifact,
